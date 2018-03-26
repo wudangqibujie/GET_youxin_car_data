1062dcbf54da11bd9be566858612a9357d800e8e
wudangqibujieshao <879303561@qq.com>
2018-03-26 17:21:58 +0800

    a

---
+++ MD5_compress.py
@@ -0,0 +1,17 @@
+import hashlib
+
+f = open("guangzhou.txt")
+g = open("md5.txt","w")
+def hash_md5(url):
+    m = hashlib.md5()
+    m.update(url.encode("utf-8"))
+    md5_url = m.hexdigest()
+    return md5_url
+
+if __name__ == '__main__':
+    for i in f:
+        print(i)
+        j = hash_md5(i)
+        g.write(j)
+f.close()
+g.close()
