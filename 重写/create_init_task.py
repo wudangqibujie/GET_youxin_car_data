39a0fe45b85e44d2a9fe144dced05c7d0119c593
wudangqibujieshao <879303561@qq.com>
2018-04-23 14:24:23 +0800

    as

---
+++ 重写/create_init_task.py
@@ -0,0 +1,28 @@
+import requests
+from lxml import etree
+from selenium import webdriver
+
+class INIT_task(object):
+    def __init__(self):
+        self.url = "https://www.xin.com/shenzhen/"
+        self.headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"}
+    def get_html(self):
+        city_url = dict()
+        brower = webdriver.Chrome()
+        brower.get(self.url)
+        html = etree.HTML(brower.page_source)
+        items = html.xpath('//table/tbody/tr')
+        print(items)
+        for i in items:
+            a = i.xpath('td[2]/dl/dd')
+            for j in a:
+                u = j.xpath('a/@href')[0]
+                n = j.xpath('a/text()')[0]
+                city_url[n] = u
+        print(city_url)
+
+
+if __name__ == '__main__':
+    i = INIT_task()
+    i.get_html()
+
